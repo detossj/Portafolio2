8a02b58bda500ce06870f2c58f1548283be72ade
deto <jorgerubilarssj@gmail.com>
2026-02-27 13:09:49 -0300

    Se agregaron componentes en HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import Skills from '@/components/Skills';
+import Experience from '@/components/Experience';
+import Education from '@/components/Education';
+import Projects from '@/components/Projects';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
+
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="homepage">
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Education />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
