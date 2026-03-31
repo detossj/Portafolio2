71cc0ff7b275096e1bd0b5bb95e9ec54f1850172
detossj <jorgerubilarssj@gmail.com>
2026-03-31 11:55:19 -0300

    Se eliminaron las rutas que no se usan en portfolioRoutes.js del backend

--- backend/src/routes/portfolioRoutes.js
+++ backend/src/routes/portfolioRoutes.js
@@ -1,31 +1,11 @@
 import express from 'express';
-import { 
-    getNav, 
-    getHero, 
-    getAbout,
-    getSkills,
-    getEducation,
-    getExperience,
-    getProjects,
-    getContact,
-    getFooter,
-    getTheme,
+import {
     getAll,
     getCV
 } from '../controllers/portfolioController.js';
 
 const router = express.Router();
 
-router.get('/nav', getNav);
-router.get('/hero', getHero);
-router.get('/about', getAbout);
-router.get('/skills', getSkills);
-router.get('/education', getEducation);
-router.get('/experience', getExperience);
-router.get('/projects', getProjects);
-router.get('/contact', getContact);
-router.get('/footer', getFooter);
-router.get('/theme', getTheme);
 router.get('/all', getAll);
 router.get('/download-cv', getCV);
 
