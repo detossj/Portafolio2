1083b8cb64dfefd8352c866bbaac7823bee69ef1
detossj <jorgerubilarssj@gmail.com>
2026-04-02 17:11:04 -0300

    Se borro configEnv y se cambio baseURL en api.config.js

--- backend/src/config/api.config.js
+++ backend/src/config/api.config.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import configEnv from './configEnv.js';
 
 export const apiClient = axios.create({
-  baseURL: configEnv.LARAVEL_API_URL,
+  baseURL: import.meta.env.VITE_LARAVEL_API_URL, 
   headers: {
     'Content-Type': 'application/json',
     'x-api-key': import.meta.env.VITE_MI_CLAVE_SECRETA
