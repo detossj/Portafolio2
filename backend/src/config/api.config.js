d745d9cd00e1972062da1119d01b76784674dc0d
detossj <jorgerubilarssj@gmail.com>
2026-04-02 16:57:03 -0300

    Se agrego de header x-api-key en api.config.jsx

--- backend/src/config/api.config.js
+++ backend/src/config/api.config.js
@@ -5,7 +5,6 @@ export const apiClient = axios.create({
   baseURL: configEnv.LARAVEL_API_URL,
   headers: {
     'Content-Type': 'application/json',
-    // Si tu Laravel requiere una API Key para que no cualquiera le pegue, la pondrías aquí:
-    // 'X-API-KEY': configEnv.MI_CLAVE_SECRETA
+    'x-api-key': configEnv.MI_CLAVE_SECRETA
   },
 });
