fce225435824867b6865c7ff1cd22b61eb238e3a
detossj <jorgerubilarssj@gmail.com>
2026-04-02 17:37:50 -0300

    Se elimino el backend de express

--- backend/src/config/api.config.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_LARAVEL_API_URL, 
-  headers: {
-    'Content-Type': 'application/json',
-    'x-api-key': import.meta.env.VITE_MI_CLAVE_SECRETA
-  },
-});
